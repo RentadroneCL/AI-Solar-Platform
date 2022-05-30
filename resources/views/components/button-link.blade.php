9158aba7dd19f68165a4e9669c1bf2a04ccbf4b4
Alvaro Farias <alvaro.farias@protonmail.com>
2022-05-29 23:55:25 -0400

    :bento: updated button component

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -1,3 +1,3 @@
-<a {{ $attributes->merge(['href' => '#', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150']) }}>
+<a {{ $attributes->merge(['href' => '#', 'class' => 'inline-flex items-center justify-center px-4 py-2 bg-blue-500 border-2 border-blue-200 rounded-lg font-semibold text-sm text-white tracking-widest hover:bg-blue-400 active:bg-blue-600 focus:outline-none focus:border-blue-600 focus:shadow-outline-blue disabled:opacity-25 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </a>
