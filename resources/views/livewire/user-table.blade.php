51195f107c71136ad6c33486e6529a1f988fd664
Alvaro Farias <alvaro.farias@protonmail.com>
2022-05-29 23:57:10 -0400

    :bento: updated user table component

--- resources/views/livewire/user-table.blade.php
+++ resources/views/livewire/user-table.blade.php
@@ -1,20 +1,23 @@
-<div class="w-full overflow-x-auto" x-data="table()" x-init="init()">
-  <div class="inline-block min-w-full overflow-hidden align-middle">
-    <table id="users-table" class="min-w-full divide-y divide-gray-200 rounded">
-      <thead class="text-xs font-medium tracking-wider text-left text-gray-600 uppercase rounded bg-gray-50">
-        <tr>
-          <th class="px-6 py-3 rounded-tl">{{ __('Name') }}</th>
-          <th class="px-6 py-3">{{ __('Email verification') }}</th>
-          <th class="px-6 py-3">{{ __('Onboarding proccess') }}</th>
-          <th class="relative px-6 py-3 rounded-tr">
-            <span class="sr-only">{{ __('Edit') }}</span>
-          </th>
-        </tr>
-        </thead>
-          <tbody class="bg-white divide-y divide-gray-200">
+<div class="w-full overflow-x-auto border bg-slate-50 rounded-xl">
+  <div class="bg-gradient-to-b from-white to-slate-100">
+    <div class="overflow-auto rounded-xl">
+      <div class="overflow-hidden shadow-sm">
+        <table x-data="userTable()" x-init="init()" id="users-table" class="w-full text-sm border-collapse table-auto">
+          <thead class="text-xs font-medium tracking-wider text-left text-gray-600 uppercase rounded bg-gray-50">
+            <tr>
+              <th class="p-4 pt-0 pb-3 pl-8 font-medium text-left border-b text-slate-400">{{ __('ID') }}</th>
+              <th class="p-4 pt-0 pb-3 font-medium text-left border-b text-slate-400">{{ __('Name') }}</th>
+              <th class="p-4 pt-0 pb-3 font-medium text-left border-b text-slate-400">{{ __('Email verification') }}</th>
+              <th class="p-4 pt-0 pb-3 pr-8 font-medium text-left border-b text-slate-400">
+                <span class="sr-only">{{ __('Actions') }}</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody class="bg-white">
             @foreach ($users as $user)
               <tr id="{{ $user->id }}">
-                <td class="px-6 py-4 whitespace-nowrap">
+                <td class="p-4 pl-8 border-b border-slate-100 text-slate-500">{{ $user->id }}</td>
+                <td class="p-4 border-b border-slate-100 text-slate-500">
                   <div class="flex items-center">
                     <div class="flex-shrink-0 w-8 h-8">
                       <img class="object-cover w-8 h-8 rounded-full" src="{{ $user->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
@@ -25,7 +28,7 @@
                     </div>
                   </div>
                 </td>
-                <td class="px-6 py-4 whitespace-nowrap">
+                <td class="p-4 border-b border-slate-100 text-slate-500">
                   @if ($user->email_verified_at)
                     <span class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">
                       {{ __('Verified') }}
@@ -36,30 +39,20 @@
                     </span>
                   @endif
                 </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  @if (! is_null($user->onboarding) && $user->onboarding->completed_at)
-                    <span class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">
-                      {{ __('Completed') }}
-                    </span>
-                  @else
-                    <span class="inline-flex px-2 text-xs font-semibold leading-5 text-yellow-800 bg-yellow-100 rounded-full">
-                      {{ __('Pending') }}
-                    </span>
-                  @endif
-                </td>
-                <td class="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                  <a class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-white border border-transparent rounded-md hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50" href="{{ route('user.edit', $user) }}" title="{{ __('Edit') }}">
-                    <i class="text-gray-400 far fa-edit"></i>
+                <td class="p-4 pr-8 border-b border-slate-100 text-slate-500">
+                  <a class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 transition duration-150 ease-in-out bg-white border border-transparent rounded-md text-slate-500 hover:bg-slate-50 hover:text-slate-700 focus:outline-none focus:bg-slate-50 active:bg-slate-50" href="{{ route('user.edit', $user) }}" title="{{ __('Edit') }}">
+                    <i class="text-slate-400 fa-solid fa-pencil fa-fw"></i>
                   </a>
                 </td>
               </tr>
-            @endforeach
-        </tbody>
-    </table>
+              @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
-
   <script>
-    const table = () => {
+    const userTable = () => {
       return {
         init() {
           this.render();
