6605c9970554230572dd4b697ae66d21db6873ac
Alvaro Farias <alvaro.farias@protonmail.com>
2022-05-29 23:58:04 -0400

    :bento: updated user sites component

--- resources/views/livewire/user-sites-management.blade.php
+++ resources/views/livewire/user-sites-management.blade.php
@@ -1,4 +1,4 @@
-<div x-data="table()" x-init="init()">
+<div>
   <x-jet-action-section>
     <x-slot name="title">
       {{ __('Site Management') }}
@@ -13,41 +13,45 @@
         <livewire:new-site-dialog-modal-form :user="$user">
       </div>
 
-      <div class="w-full overflow-x-auto">
-        <div class="inline-block min-w-full overflow-hidden align-middle">
-          <table id="sites-table" class="min-w-full divide-y divide-gray-200 rounded">
-            <thead class="text-xs font-medium tracking-wider text-left text-gray-600 uppercase rounded bg-gray-50">
-              <tr>
-                <th class="px-6 py-3">{{ __('Name') }}</th>
-                <th class="px-6 py-3">{{ __('Commissioning date') }}</th>
-                <th class="relative px-6 py-3 rounded-tr">
-                  <span class="sr-only">{{ __('Manage') }}</span>
-                </th>
-              </tr>
-            </thead>
-            <tbody class="bg-white divide-y divide-gray-200">
-              @foreach ($sites as $site)
-                <tr id="{{ $site->id }}">
-                  <td class="px-6 py-4 whitespace-nowrap">{{ $site->name }}</td>
-                  <td class="px-6 py-4 whitespace-nowrap">
-                    {{ $site->commissioning_date->toDateString() }}
-                  </td>
-                  <td class="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                    <a class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-white border border-transparent rounded-md hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50" href="{{ route('site.show', $site) }}" title="{{ __('Manage') }}">
-                      <i class="text-gray-400 fas fa-cog fa-fw"></i>
-                    </a>
-                  </td>
-                </tr>
-              @endforeach
-            </tbody>
-          </table>
+      <div class="w-full overflow-x-auto border bg-slate-50 rounded-xl">
+        <div class="bg-gradient-to-b from-white to-slate-100">
+          <div class="overflow-auto rounded-xl">
+            <div class="overflow-hidden shadow-sm">
+              <table x-data="siteTable()" x-init="init()" id="sites-table" class="w-full text-sm border-collapse table-auto">
+                <thead class="text-xs font-medium tracking-wider text-left text-gray-600 uppercase rounded bg-gray-50">
+                  <tr>
+                    <th class="p-4 pt-0 pb-3 pl-8 font-medium text-left border-b text-slate-400">{{ __('ID') }}</th>
+                    <th class="p-4 pt-0 pb-3 font-medium text-left border-b text-slate-400">{{ __('Name') }}</th>
+                    <th class="p-4 pt-0 pb-3 font-medium text-left border-b text-slate-400">{{ __('Commissioning date') }}</th>
+                    <th class="p-4 pt-0 pb-3 pr-8 font-medium text-left border-b text-slate-400">
+                      <span class="sr-only">{{ __('Actions') }}</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody class="bg-white">
+                  @foreach ($sites as $site)
+                    <tr id="{{ $site->id }}">
+                      <td class="p-4 pl-8 border-b border-slate-100 text-slate-500">{{ $site->id }}</td>
+                      <td class="p-4 border-b border-slate-100 text-slate-500">{{ $site->name }}</td>
+                      <td class="p-4 border-b border-slate-100 text-slate-500">{{ $site->commissioning_date->toDateString() }}</td>
+                      <td class="p-4 pr-8 border-b border-slate-100 text-slate-500">
+                        <a class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 transition duration-150 ease-in-out bg-white border border-transparent rounded-md text-slate-500 hover:bg-slate-50 hover:text-slate-700 focus:outline-none focus:bg-slate-50 active:bg-slate-50" href="{{ route('site.show', $site) }}" title="{{ __('Manage') }}">
+                          <i class="text-slate-400 fa-solid fa-pencil fa-fw"></i>
+                        </a>
+                      </td>
+                    </tr>
+                  @endforeach
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </x-slot>
   </x-jet-action-section>
 
   <script>
-    const table = () => {
+    const siteTable = () => {
       return {
         init() {
           this.render();
