aaadfc4a4b16006527e764127239c104b6145ec1
Alvaro Farias <alvaro.farias@protonmail.com>
2022-06-26 18:05:07 -0400

    :alien: refactor user table, due to api changes

--- app/Http/Livewire/UserTable.php
+++ app/Http/Livewire/UserTable.php
@@ -2,15 +2,64 @@
 
 namespace App\Http\Livewire;
 
-use Livewire\Component;
-use Illuminate\Database\Eloquent\Collection;
-
-class UserTable extends Component
+use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Columns\LinkColumn;
+use Rappasoft\LaravelLivewireTables\Views\Columns\BooleanColumn;
+use Rappasoft\LaravelLivewireTables\Views\Columns\ButtonGroupColumn;
+class UserTable extends DataTableComponent
 {
     /**
-     * Users collection.
+     * Calls the query method on the model.
      *
-     * @var Collection
+     * @return Builder
      */
-    public Collection $users;
+    public function builder(): Builder
+    {
+        return User::query()
+            ->select('id', 'name', 'email', 'email_verified_at');
+    }
+
+    /**
+     * Component configuration.
+     *
+     * @return void
+     */
+    public function configure(): void
+    {
+        $this->setPrimaryKey('id');
+    }
+
+    /**
+     * Column objects in the order you wish to see them on the table.
+     *
+     * @return array
+     */
+    public function columns(): array
+    {
+        return [
+            Column::make("Id", "id")
+                ->sortable(),
+            Column::make("Name", "name")
+                ->sortable()
+                ->searchable(),
+            Column::make("Email", "email")
+                ->sortable()
+                ->searchable(),
+            BooleanColumn::make('Verified', 'email_verified_at'),
+            ButtonGroupColumn::make(__('Actions'))
+                ->buttons([
+                    LinkColumn::make(__('Manage'))
+                        ->title(fn($row) => __('Manage'))
+                        ->attributes(function ($row) {
+                            return [
+                                'class' => 'text-slate-400 hover:text-slate-500 hover:underline',
+                            ];
+                        })
+                        ->location(fn($row) => route('user.edit', $row)),
+                ]),
+        ];
+    }
 }
