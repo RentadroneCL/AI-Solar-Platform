91a1dd426c0cb00f572a12bb30214da795012527
Alvaro Farias <alvaro.farias@protonmail.com>
2022-11-16 11:30:44 -0300

    ✨ Annotation related changes

--- app/Http/Livewire/Annotations.php
+++ app/Http/Livewire/Annotations.php
@@ -2,15 +2,43 @@
 
 namespace App\Http\Livewire;
 
-use App\Models\Annotation;
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\View\View;
 use Livewire\Component;
+use Illuminate\Support\Arr;
+use App\Models\{Annotation, User};
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
 
 class Annotations extends Component
 {
+    /**
+     * Undocumented variable
+     *
+     * @var boolean
+     */
+    public bool $currentTeamIsEmpty = false;
+
+    /**
+     * Annotation form.
+     *
+     * @var boolean
+     */
+    public bool $displayAnnotationCreationForm = false;
+
+    /**
+     * User's accounts suggestions.
+     *
+     * @var boolean
+     */
+    public bool $displaySuggestions = false;
+
+    /**
+     * Assignees list.
+     *
+     * @var boolean
+     */
+    public bool $displayAssignees = false;
+
     /**
      * The given model
      *
@@ -26,6 +54,27 @@ class Annotations extends Component
     public Collection $annotations;
 
     /**
+     * Suggestion list.
+     *
+     * @var Collection
+     */
+    public Collection $suggestions;
+
+    /**
+     * Assignees.
+     *
+     * @var \Illuminate\Support\Collection
+     */
+    public \Illuminate\Support\Collection $assignees;
+
+    /**
+     * Search criteria.
+     *
+     * @var string
+     */
+    public string $query = '';
+
+    /**
      * The component's state.
      *
      * @var array
@@ -33,7 +82,17 @@ class Annotations extends Component
     public array $state = [
         'user_id' => '',
         'content' => '',
-        'custom_properties' => null,
+        'custom_properties' => [
+            'title' => '',
+            'assignees' => [],
+            'status' => '',
+            'priority' => '',
+            'commissioning_at' => null,
+            'feature' => [
+                'values' => null,
+                'geometry' => null,
+            ],
+        ],
     ];
 
     /**
@@ -43,7 +102,20 @@ class Annotations extends Component
      */
     protected array $rules = [
         'state.content' => 'required|string',
-        'state.custom_properties' => 'nullable|string',
+        'state.custom_properties.title' => 'required|string',
+        'state.custom_properties.assignees' => 'required|array',
+        'state.custom_properties.status' => 'required|string',
+        'state.custom_properties.priority' => 'required|string',
+        'state.custom_properties.commissioning_at' => 'required|date',
+    ];
+
+    /**
+     * Event Listeners.
+     *
+     * @var array
+     */
+    protected $listeners = [
+        'featureAtPixel' => 'setFeatureAtPixel',
     ];
 
     /**
@@ -54,20 +126,120 @@ class Annotations extends Component
     public function mount(): void
     {
         $this->state['user_id'] = Auth::id();
-    }
 
-    public function create(): Annotation
-    {
+        // Get all of the team's users, excluding the owner...
+        $this->suggestions = Auth::user()->currentTeam->users;
 
+        $this->currentTeamIsEmpty = $this->suggestions->isEmpty();
+
+        $this->assignees = collect([]);
     }
 
     /**
-     * Render the component.
+     * Feature at pixel.
      *
-     * @return View
+     * @param array $payload - feature json object.
+     *
+     * @return void
      */
-    public function render(): View
+    public function setFeatureAtPixel(array $payload = []): void
     {
-        return view('livewire.annotations');
+        if (Arr::exists($payload, 'values_')) {
+            $this->state['custom_properties']['feature']['values'] = Arr::except($payload['values_'], ['geometry']);
+        }
+
+        if (Arr::has($payload, 'values_.geometry')) {
+            $this->state['custom_properties']['feature']['geometry'] = Arr::get($payload, 'values_.geometry');
+        }
+    }
+
+    /**
+     * Set default state.
+     *
+     * @return void
+     */
+    public function resetState(): void
+    {
+        $this->state = [
+            'user_id' => Auth::id(),
+            'content' => '',
+            'custom_properties' => [
+                'title' => '',
+                'assignees' => [],
+                'status' => '',
+                'priority' => '',
+                'commissioning_at' => null,
+                'feature' => [
+                    'values' => null,
+                    'geometry' => null,
+                ],
+            ],
+        ];
+
+        $this->suggestions = Auth::user()->currentTeam->users;
+
+        $this->assignees = collect([]);
+    }
+
+    /**
+     * Add assigned to list.
+     *
+     * @param User $user
+     * @return void
+     */
+    public function toggleAssigned(User $user): void
+    {
+        $this->assignees->push($user);
+        $this->displayAssignees = true;
+
+        // Update suggestions list state
+        $this->suggestions = $this->suggestions
+            ->except($this->assignees->pluck('id')->toArray());
+    }
+
+    /**
+     * Remove assigned from list.
+     *
+     * @param User $user
+     * @return void
+     */
+    public function removeAssigned(User $user): void
+    {
+        $this->assignees = $this->assignees->filter(
+            fn($item) => $item['id'] !== $user->id
+        );
+
+        $this->suggestions->push($user);
+
+        if ($this->assignees->count() === 0) {
+            $this->displayAssignees = false;
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Annotation
+     */
+    public function store(): Annotation
+    {
+        $this->state['custom_properties']['assignees'] = $this->assignees->toArray();
+
+        $this->resetErrorBag();
+
+        $this->validate();
+
+        $annotation = $this->model->annotation()->create($this->state);
+
+        $this->resetState();
+
+        $this->displayAnnotationCreationForm = false;
+
+        $this->dispatchBrowserEvent('alert', [
+            'type' => 'success',
+            'message' => __('Successfully saved annotation.')
+        ]);
+
+        return $annotation;
     }
 }
--- app/Models/Annotation.php
+++ app/Models/Annotation.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Traits\CustomProperties;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Annotation extends Model
 {
-    use HasFactory;
+    use HasFactory, CustomProperties;
 
     /**
      * The table associated with the model.
@@ -29,6 +32,24 @@ class Annotation extends Model
     ];
 
     /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'custom_properties' => 'array',
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'custom_properties->commissioning_at',
+    ];
+
+    /**
      * Annotations related models.
      *
      * @return MorphTo
@@ -36,5 +57,15 @@ class Annotation extends Model
     public function annotable(): MorphTo
     {
         return $this->morphTo();
+    }
+
+    /**
+     * User related mode.
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
