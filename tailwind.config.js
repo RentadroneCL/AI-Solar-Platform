077d887034f55077d3cd004d0f0274953d6aa54f
Alvaro Farias <alvaro.farias@protonmail.com>
2022-11-16 10:36:48 -0300

    🔧 updated config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,24 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './vendor/rappasoft/laravel-livewire-tables/resources/views/**/*.blade.php',
-    ],
-
-    darkMode: 'class',
-
-    theme: {
-        fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
+  content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './vendor/laravel/jetstream/**/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+    './resources/views/vendor/jetstream/components/*.blade.php',
+    './vendor/rappasoft/laravel-livewire-tables/resources/views/**/*.blade.php',
+  ],
+  darkMode: 'class',
+  theme: {
+    fontFamily: {
+      sans: ['Nunito', ...defaultTheme.fontFamily.sans],
     },
-
-    variants: {
-        extend: {
-            opacity: ['disabled'],
-        },
+  },
+  variants: {
+    extend: {
+      opacity: ['disabled'],
     },
-
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 };
